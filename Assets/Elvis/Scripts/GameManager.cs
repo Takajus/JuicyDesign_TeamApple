fceebebae03623f6526dafe729efe304808963b8
Elvis Henry <elvishenry2402@gmail.com>
2022-11-17 09:34:39 +0100

    Sound Manager
    Add theme sound

--- Assets/Elvis/Scripts/GameManager.cs
+++ Assets/Elvis/Scripts/GameManager.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     private static GameManager _instance;
+    private SoundManager _soundManager;
     
     private void Awake()
     {
@@ -16,5 +18,13 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
+        
+        _soundManager = FindObjectOfType<SoundManager>();_soundManager = FindObjectOfType<SoundManager>();
+
+    }
+
+    private void Start()
+    {
+        _soundManager.PlaySound("Theme");
     }
 }
