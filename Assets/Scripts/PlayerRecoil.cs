b3056789a3cee5640e1a91869d30190f63f7c74d
Paul Putegnat <paul.putegnat@edu.devinci.fr>
2022-11-22 09:23:12 +0100

    Failed attempt at making a recoil

--- Assets/Scripts/PlayerRecoil.cs
+++ Assets/Scripts/PlayerRecoil.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerRecoil : MonoBehaviour
 {
-    [Range(0f, 1f)] public float moveBackDistance = 1f;
+    [Range(0f, 3f)] public float moveBackDistance = 1f;
     [Range(0f, 2f)] public float recoilTime;
+    [Range(0f, 2f)] public float comeBackTime;
     public GameObject Player;
 
     private Vector3 _targetedPosition;
@@ -13,45 +14,65 @@ public class PlayerRecoil : MonoBehaviour
     private bool _methodSettings = false;
 
     private float _timer = 0f;
-    private float _percent;
+    private float _firstPercent;
+    private float _secondPercent;
 
     void Update()
     {
         _timer += Time.deltaTime;
-        _percent = _timer / recoilTime;
+        _firstPercent = _timer / recoilTime;
+        _secondPercent = _timer / comeBackTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            if (!_methodSettings)
-            {
-                _methodSettings = true;
-                _targetedPosition.x = Player.transform.position.x;
-                _targetedPosition.y = Player.transform.position.y - moveBackDistance;
-                _targetedPosition.z = Player.transform.position.z;
-                _currentPosition = Player.transform.position;
-            }
-            Recoil(recoilTime);
+            RecoilSin();
+            //if (!_methodSettings)
+            //{
+            //    _methodSettings = true;
+            //    _targetedPosition.x = Player.transform.position.x;
+            //    _targetedPosition.y = Player.transform.position.y - moveBackDistance;
+            //    _targetedPosition.z = Player.transform.position.z;
+            //    _currentPosition = Player.transform.position;
+            //}
+            //if (Player.transform.position != _targetedPosition)
+            //{
+            //    Recoil();
+            //}
+            //else
+            //{
+            //    ComeBack();
+            //}
+            //TimeOver();
         }
     }
     
-    public void Recoil(float lerpTime)
+    public void Recoil()
+    {
+        Debug.Log("Recoil");
+        Vector3 currentPosition = _currentPosition;
+        Vector3 targetedPosition = _targetedPosition;
+        
+        Player.transform.position = Vector3.Lerp(currentPosition, targetedPosition, _firstPercent);
+    }
+
+    public void RecoilSin()
+    {
+        Player.transform.position = new Vector3(0, Mathf.Sin(_timer), 0);
+    }
+
+    public void ComeBack()
     {
         Debug.Log("Recoil");
         Vector3 currentPosition = _currentPosition;
         Vector3 targetedPosition = _targetedPosition;
 
-        if(_percent < recoilTime / 2)
-        {
-            Player.transform.position = Vector3.Lerp(currentPosition, targetedPosition, _percent);
-        }
-        else
-        {
-            Player.transform.position = Vector3.Lerp(targetedPosition, currentPosition, _percent);
-        }
+        Player.transform.position = Vector3.Lerp(targetedPosition, currentPosition, _secondPercent);
+    }
 
-        if(_timer >= lerpTime)
-        {
-            _methodSettings = false;
-            _timer = 0;
-        }
+    public void TimeOver()
+    {
+        if(_timer >= recoilTime + comeBackTime)
+        _timer = 0;
+        _methodSettings = false;
     }
 }
