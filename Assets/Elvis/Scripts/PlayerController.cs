919133b2e2c76b73e9e298e6130f12a62ed1e909
Elvis Henry <elvishenry2402@gmail.com>
2022-11-24 10:19:50 +0100

    BFG
    FIX reset shoot

--- Assets/Elvis/Scripts/PlayerController.cs
+++ Assets/Elvis/Scripts/PlayerController.cs
@@ -84,16 +84,17 @@ public class PlayerController : MonoBehaviour
         else if (Input.GetButtonDown("Fire2"))
         {
             Shoting(_bfg, "BFGAvailable");
-            //if (_canUseBfg)
-            //{
-            //    Shoting(_bfg, "BFGAvailable");
-            //    _canUseBfg = false;
-            //}
-            //else
-            //{
-            //    JuicyManager.Instance.PopUpScoreSystem(gameObject, "Can not use BFG");
-            //    SoundManager.Instance.PlaySound("BFGUnavailable");
-            //}
+            
+            if (_canUseBfg)
+            {
+                Shoting(_bfg, "BFGAvailable");
+                _canUseBfg = false;
+            }
+            else
+            {
+                JuicyManager.Instance.PopUpScoreSystem(gameObject, "Can not use BFG");
+                SoundManager.Instance.PlaySound("BFGUnavailable");
+            }
         }
 
         // StartCoroutine(ShotTimer());
